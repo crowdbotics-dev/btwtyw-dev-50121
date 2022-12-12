ccc24a9734b44c7c086baf0b4e084ebd96e38d0d
Crowdbotics <engineering@crowdbotics.com>
2022-12-12 09:59:28 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/btwtyw_dev_50121/MainActivity.java
@@ -0,0 +1,15 @@
+package com.btwtyw_dev_50121;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "btwtyw_dev_50121";
+  }
+}
